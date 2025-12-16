ebc028534b1bdad457b96acaf4d6e9f7ef34bc50
yeseul0 <choiyeseul@g.hongik.ac.kr>
2025-12-17 02:47:13 +0900

    PropertyToken function call

---
+++ frontend_v2/src/apis/ABIs.ts
@@ -0,0 +1,880 @@
+export const PROPERTY_TOKEN_ABI = [
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "_owner",
+						"type": "address"
+					},
+					{
+						"internalType": "string",
+						"name": "_name",
+						"type": "string"
+					},
+					{
+						"internalType": "string",
+						"name": "_symbol",
+						"type": "string"
+					},
+					{
+						"internalType": "bytes32",
+						"name": "_propertyId",
+						"type": "bytes32"
+					},
+					{
+						"internalType": "uint256",
+						"name": "_maxSupply",
+						"type": "uint256"
+					},
+					{
+						"internalType": "address",
+						"name": "_identityRegistry",
+						"type": "address"
+					},
+					{
+						"internalType": "address",
+						"name": "_compliance",
+						"type": "address"
+					},
+					{
+						"internalType": "address",
+						"name": "_paymentToken",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "_tokenPrice",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "nonpayable",
+				"type": "constructor"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": true,
+						"internalType": "address",
+						"name": "owner",
+						"type": "address"
+					},
+					{
+						"indexed": true,
+						"internalType": "address",
+						"name": "spender",
+						"type": "address"
+					},
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "value",
+						"type": "uint256"
+					}
+				],
+				"name": "Approval",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": true,
+						"internalType": "address",
+						"name": "compliance",
+						"type": "address"
+					}
+				],
+				"name": "ComplianceSet",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": true,
+						"internalType": "address",
+						"name": "registry",
+						"type": "address"
+					}
+				],
+				"name": "IdentityRegistrySet",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": true,
+						"internalType": "address",
+						"name": "propertyOwner",
+						"type": "address"
+					},
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					}
+				],
+				"name": "InitialMinted",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": false,
+						"internalType": "address",
+						"name": "account",
+						"type": "address"
+					}
+				],
+				"name": "Paused",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": true,
+						"internalType": "uint256",
+						"name": "snapshotId",
+						"type": "uint256"
+					},
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "totalSupply",
+						"type": "uint256"
+					}
+				],
+				"name": "SnapshotCreated",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "oldPrice",
+						"type": "uint256"
+					},
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "newPrice",
+						"type": "uint256"
+					}
+				],
+				"name": "TokenPriceUpdated",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": true,
+						"internalType": "address",
+						"name": "buyer",
+						"type": "address"
+					},
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					},
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "cost",
+						"type": "uint256"
+					}
+				],
+				"name": "TokensPurchased",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": true,
+						"internalType": "address",
+						"name": "from",
+						"type": "address"
+					},
+					{
+						"indexed": true,
+						"internalType": "address",
+						"name": "to",
+						"type": "address"
+					},
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "value",
+						"type": "uint256"
+					}
+				],
+				"name": "Transfer",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": false,
+						"internalType": "address",
+						"name": "account",
+						"type": "address"
+					}
+				],
+				"name": "Unpaused",
+				"type": "event"
+			},
+			{
+				"anonymous": false,
+				"inputs": [
+					{
+						"indexed": true,
+						"internalType": "address",
+						"name": "to",
+						"type": "address"
+					},
+					{
+						"indexed": false,
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					}
+				],
+				"name": "Withdrawn",
+				"type": "event"
+			},
+			{
+				"inputs": [],
+				"name": "PERMIT2",
+				"outputs": [
+					{
+						"internalType": "contract IPermit2",
+						"name": "",
+						"type": "address"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "",
+						"type": "address"
+					},
+					{
+						"internalType": "address",
+						"name": "",
+						"type": "address"
+					}
+				],
+				"name": "allowance",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "spender",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					}
+				],
+				"name": "approve",
+				"outputs": [
+					{
+						"internalType": "bool",
+						"name": "",
+						"type": "bool"
+					}
+				],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "",
+						"type": "address"
+					}
+				],
+				"name": "balanceOf",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "account",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "snapshotId",
+						"type": "uint256"
+					}
+				],
+				"name": "balanceOfAt",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "from",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					}
+				],
+				"name": "burn",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					}
+				],
+				"name": "buy",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "nonce",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "deadline",
+						"type": "uint256"
+					},
+					{
+						"internalType": "bytes",
+						"name": "signature",
+						"type": "bytes"
+					}
+				],
+				"name": "buyWithPermit",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "compliance",
+				"outputs": [
+					{
+						"internalType": "contract IModularCompliance",
+						"name": "",
+						"type": "address"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "currentSnapshotId",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "from",
+						"type": "address"
+					},
+					{
+						"internalType": "address",
+						"name": "to",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					}
+				],
+				"name": "forcedTransfer",
+				"outputs": [
+					{
+						"internalType": "bool",
+						"name": "",
+						"type": "bool"
+					}
+				],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "getContractBalance",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					}
+				],
+				"name": "getCost",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "identityRegistry",
+				"outputs": [
+					{
+						"internalType": "contract IIdentityRegistry",
+						"name": "",
+						"type": "address"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "propertyOwner",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "ownerAmount",
+						"type": "uint256"
+					}
+				],
+				"name": "initialMint",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "initialized",
+				"outputs": [
+					{
+						"internalType": "bool",
+						"name": "",
+						"type": "bool"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "maxSupply",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "to",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					}
+				],
+				"name": "mint",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "name",
+				"outputs": [
+					{
+						"internalType": "string",
+						"name": "",
+						"type": "string"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "owner",
+				"outputs": [
+					{
+						"internalType": "address",
+						"name": "",
+						"type": "address"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "pause",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "paused",
+				"outputs": [
+					{
+						"internalType": "bool",
+						"name": "",
+						"type": "bool"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "paymentToken",
+				"outputs": [
+					{
+						"internalType": "contract IERC20",
+						"name": "",
+						"type": "address"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "propertyId",
+				"outputs": [
+					{
+						"internalType": "bytes32",
+						"name": "",
+						"type": "bytes32"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "propertyURI",
+				"outputs": [
+					{
+						"internalType": "string",
+						"name": "",
+						"type": "string"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "remainingSupply",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "_compliance",
+						"type": "address"
+					}
+				],
+				"name": "setCompliance",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "registry",
+						"type": "address"
+					}
+				],
+				"name": "setIdentityRegistry",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "string",
+						"name": "uri",
+						"type": "string"
+					}
+				],
+				"name": "setPropertyURI",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "newPrice",
+						"type": "uint256"
+					}
+				],
+				"name": "setTokenPrice",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "snapshot",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "snapshotId",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "symbol",
+				"outputs": [
+					{
+						"internalType": "string",
+						"name": "",
+						"type": "string"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "tokenPrice",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "totalSupply",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "uint256",
+						"name": "snapshotId",
+						"type": "uint256"
+					}
+				],
+				"name": "totalSupplyAt",
+				"outputs": [
+					{
+						"internalType": "uint256",
+						"name": "",
+						"type": "uint256"
+					}
+				],
+				"stateMutability": "view",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "to",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					}
+				],
+				"name": "transfer",
+				"outputs": [
+					{
+						"internalType": "bool",
+						"name": "",
+						"type": "bool"
+					}
+				],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "from",
+						"type": "address"
+					},
+					{
+						"internalType": "address",
+						"name": "to",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					}
+				],
+				"name": "transferFrom",
+				"outputs": [
+					{
+						"internalType": "bool",
+						"name": "",
+						"type": "bool"
+					}
+				],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [],
+				"name": "unpause",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			},
+			{
+				"inputs": [
+					{
+						"internalType": "address",
+						"name": "to",
+						"type": "address"
+					},
+					{
+						"internalType": "uint256",
+						"name": "amount",
+						"type": "uint256"
+					}
+				],
+				"name": "withdraw",
+				"outputs": [],
+				"stateMutability": "nonpayable",
+				"type": "function"
+			}
+];
---
+++ frontend_v2/src/apis/blockchain.ts
@@ -0,0 +1,225 @@
+import { BrowserProvider, Contract } from 'ethers' 
+import { PROPERTY_TOKEN_ABI } from './ABIs'
+
+// ============================================
+//          1. 메타마스크 연결 (topbar 가져옴)
+// ============================================
+export const getProvider = async (): Promise<BrowserProvider> => {
+  if (typeof window.ethereum === 'undefined') {
+    throw new Error('MetaMask가 설치되지 않았습니다')
+  }
+  return new BrowserProvider(window.ethereum)
+}
+
+/**
+ * 연결된 지갑 주소 가져오기
+ * Topbar.tsx 패턴 그대로 재사용
+ */
+export const getWalletAddress = async (): Promise<string> => {
+  const provider = await getProvider()
+  const accounts = await provider.listAccounts()
+
+  if (accounts.length === 0) {
+    throw new Error('연결된 지갑이 없습니다')
+  }
+
+  return accounts[0].address
+}
+
+// ============================================
+//       3. 컨트랙트 인스턴스 생성
+// ============================================
+/**
+ * PropertyToken 컨트랙트 인스턴스 가져오기
+ * @param contractAddress 백엔드에서 받은 컨트랙트 주소
+ */
+export const getPropertyContract = async (
+    contractAddress: string
+  ): Promise<Contract> => {
+    const provider = await getProvider()
+    return new Contract(contractAddress, PROPERTY_TOKEN_ABI, provider)
+  }
+
+// ============================================
+//       4. 부동산 기본 정보 조회
+//          (지갑 연결 불필요)
+// ============================================
+//부동산 기본 정보 가져오기
+export interface PropertyBasicInfo {
+  name: string                  // "제주 오션빌리지"
+  symbol: string                // "JEJU"
+  propertyId: string            // bytes32를 string으로
+  maxSupply: string             // 최대 발행량
+  totalSupply: string           // 현재 발행량
+  remainingSupply: string       // 남은 수량
+  tokenPrice: string            // 토큰 1개 가격 (KRWT)
+  contractAddress: string       // 컨트랙트 주소
+  owner: string                 // 관리자 주소
+  paymentToken: string          // KRWT 컨트랙트 주소
+  identityRegistry: string      // 신원 검증 컨트랙트
+  compliance: string            // 컴플라이언스 컨트랙트
+  paused: boolean               // 일시정지 여부
+  initialized: boolean          // 초기화 여부
+}
+/**
+ * 부동산 기본 정보 가져오기
+ * 지갑 연결 없이도 조회 가능 (public view 함수들)
+ * @param contractAddress 컨트랙트 주소
+ */
+export const getPropertyBasicInfo = async (contractAddress: string): Promise<PropertyBasicInfo> => {
+  try {
+    const contract = await getPropertyContract(contractAddress)
+
+    // Promise.all로 병렬 처리 (빠름!)
+    const [
+      name,
+      symbol,
+      propertyId,
+      maxSupply,
+      totalSupply,
+      remainingSupply,
+      tokenPrice,
+      owner,
+      paymentToken,
+      identityRegistry,
+      compliance,
+      paused,
+      initialized,
+    ] = await Promise.all([
+      contract.name(),
+      contract.symbol(),
+      contract.propertyId(),
+      contract.maxSupply(),
+      contract.totalSupply(),
+      contract.remainingSupply(),
+      contract.tokenPrice(),
+      contract.owner(),
+      contract.paymentToken(),
+      contract.identityRegistry(),
+      contract.compliance(),
+      contract.paused(),
+      contract.initialized(),
+    ])
+
+    return {
+      name,
+      symbol,
+      propertyId,                           // bytes32 그대로 string으로 변환됨
+      maxSupply: maxSupply.toString(),      // bigint → string
+      totalSupply: totalSupply.toString(),
+      remainingSupply: remainingSupply.toString(),
+      tokenPrice: tokenPrice.toString(),
+      contractAddress,
+      owner,
+      paymentToken,
+      identityRegistry,
+      compliance,
+      paused,
+      initialized,
+    }
+  } catch (error) {
+    console.error('부동산 기본 정보 조회 실패:', error)
+    throw error
+  }
+}
+// ============================================
+//       5. 사용자 보유량 조회
+//          (지갑 연결 필요)
+// ============================================
+/**
+ * 사용자의 토큰 보유량 조회
+ * @param contractAddress 컨트랙트 주소
+ * @param userAddress 조회할 사용자 주소 (없으면 현재 연결된 지갑)
+ */
+export const getUserBalance = async (
+  contractAddress: string,
+  userAddress?: string
+): Promise<string> => {
+  const contract = await getPropertyContract(contractAddress)
+
+  // userAddress 없으면 현재 지갑 주소 사용
+  const address = userAddress || await getWalletAddress()
+
+  const balance = await contract.balanceOf(address)
+  return balance.toString()
+}
+
+
+// ============================================
+//       6. 전체 정보 한번에 조회
+//          (지갑 연결 필요)
+// ============================================
+export interface PropertyFullInfo extends PropertyBasicInfo {
+  userBalance: string           // 사용자 보유량
+  userAddress: string           // 사용자 지갑 주소
+}
+/**
+ * 부동산 정보 + 사용자 보유량 한번에 가져오기
+ * 포트폴리오, 구매 패널 등에서 사용
+ * @param contractAddress 컨트랙트 주소
+ */
+//부동 산정보 + 사용자 보유량
+export const getPropertyFullInfo = async (contractAddress: string): Promise<PropertyFullInfo> => {
+  try {
+    const contract = await getPropertyContract(contractAddress)
+    const userAddress = await getWalletAddress()
+
+    // 모든 정보 한번에 가져오기 (병렬 처리)
+    const [
+      name,
+      symbol,
+      propertyId,
+      maxSupply,
+      totalSupply,
+      remainingSupply,
+      tokenPrice,
+      owner,
+      paymentToken,
+      identityRegistry,
+      compliance,
+      paused,
+      initialized,
+      userBalance,  // 사용자 보유량 추가!
+    ] = await Promise.all([
+      contract.name(),
+      contract.symbol(),
+      contract.propertyId(),
+      contract.maxSupply(),
+      contract.totalSupply(),
+      contract.remainingSupply(),
+      contract.tokenPrice(),
+      contract.owner(),
+      contract.paymentToken(),
+      contract.identityRegistry(),
+      contract.compliance(),
+      contract.paused(),
+      contract.initialized(),
+      contract.balanceOf(userAddress),  // 사용자 보유량
+    ])
+
+    return {
+      // 부동산 정보
+      name,
+      symbol,
+      propertyId,
+      maxSupply: maxSupply.toString(),
+      totalSupply: totalSupply.toString(),
+      remainingSupply: remainingSupply.toString(),
+      tokenPrice: tokenPrice.toString(),
+      contractAddress,
+      owner,
+      paymentToken,
+      identityRegistry,
+      compliance,
+      paused,
+      initialized,
+
+      // 사용자 정보
+      userBalance: userBalance.toString(),
+      userAddress,
+    }
+  } catch (error) {
+    console.error('전체 정보 조회 실패:', error)
+    throw error
+  }
+}
